54c7a0aa18381aeac1613df9b3e2aa593592e0c4
Alejandro-Progam <60205337+Alejandro-Progam@users.noreply.github.com>
2020-01-27 19:33:43 -0500

    Add files via upload

---
+++ desafio01-04-4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Desafío01_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese variable w");
+            double w = double.Parse(Console.ReadLine()) ;
+            Console.WriteLine("Ingrese variable t");
+            double t = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese variable c");
+            double c = double.Parse(Console.ReadLine());
+            // primero hay que hallar el lado y
+            double y = Math.Cos(c * Math.PI / 180)*t;
+            //luego hallo el lado z 
+            double z = Math.Sin(c * Math.PI / 180) * t;
+            // luego utilizo teorema de pitagoras 
+            double n = Math.Pow(w, 2) - Math.Pow(z, 2);
+            double x = Math.Sqrt(n) - y;
+            Console.Write("x=" + x);
+
+        
+        }
+    }
+}
