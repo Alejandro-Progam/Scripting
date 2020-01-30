d5c0a7cc7488027c28c2732e5b3acdab088ec2c3
Alejandro-Progam <60205337+Alejandro-Progam@users.noreply.github.com>
2020-01-29 21:05:01 -0500

    Add files via upload

---
+++ Ejercicio-01.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba su salario (COP)");
+            int s = int.Parse(Console.ReadLine());
+              if (s< 1755606)
+             {
+                Console.WriteLine("Tarifa A");
+             }
+              else if (s >= 1755606 && s<3511212)
+            {
+                Console.WriteLine("Tarifa B");
+            }
+              else {
+                Console.WriteLine("Tarifa C");
+
+                
+            }
+        }
+    }
+}
---
+++ Ejercicio-02.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba su salario (COP)");
+            int s = int.Parse(Console.ReadLine());
+            if (s < 1755606)
+            {
+                Console.WriteLine("Su cuota a pagra es: 3400");
+            }
+            else if (s >= 1755606 && s < 4389015)
+            {
+                Console.WriteLine("Su cuota a pagar es 12700");
+            }
+            else
+            {
+                Console.WriteLine("Su cuota a pagar es 33500");
+
+
+            }
+        }
+    }
+}
