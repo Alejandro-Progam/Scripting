e2578e3af1bce8e841f66c262877b15af02d817c
Alejandro-Progam <60205337+Alejandro-Progam@users.noreply.github.com>
2020-02-20 09:41:42 -0500

    Add files via upload

---
+++ actividad_clase_diez.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main()
+        {
+            int cont = 0;
+            Random aleatorio = new Random();
+            while (true)
+            {
+                int resultado, c = 0;
+
+                int a = aleatorio.Next(0, 11);
+                Console.WriteLine("el numero a es : " + a);
+                int b = aleatorio.Next(0, 11);
+                Console.WriteLine("el numero b es : " + b);
+                c = a + b;
+                Console.WriteLine("escriba la suma:");
+                resultado = int.Parse(Console.ReadLine());
+                if (resultado == c)
+                {
+                    Console.WriteLine("puede continuar");
+                    break;
+                }
+                cont++;
+                if (cont == 3)
+                {
+                    Console.WriteLine("no se ha podido iniciar el programa");
+                    break;
+                }
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
