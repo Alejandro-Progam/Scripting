a4ccb3aab3ed3679e98c087692cca666d33bb035
Alejandro-Progam <60205337+Alejandro-Progam@users.noreply.github.com>
2020-03-05 09:51:56 -0500

    Add files via upload

---
+++ Trabajo clase 14.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra = "benito";
+            string frase = " camelo benito benito benito";
+            int posicion = 0;
+            string fraseFormada = "";
+            palabra = palabra.ToUpper();
+            frase = frase.ToUpper();
+
+            for(int i=0; i < (frase.Length - palabra.Length+1); i++)
+            {
+                fraseFormada = "";
+
+                for (int j =0; j < palabra.Length; j++)
+                {
+                    if (frase[j+i]==palabra[j])
+                    {
+                        char letra = frase[j+i];
+                        fraseFormada += letra;
+                        posicion = j;
+                       
+                    }
+
+                    if (fraseFormada == palabra)
+                    {
+                        
+                        Console.WriteLine("palabra encontrada en la posicion "+i);
+
+                    }
+                }
+                
+                
+
+            }
+
+        }
+    }
+}
